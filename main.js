62de824a6d21c84b04b81c784a6ce5c0403c7f8d
Avitej <82682098+Avitej@users.noreply.github.com>
2022-12-18 16:57:49 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 inputVar = "";
 status = "";
+objects = [];
 
 function setup() {
     canvas = createCanvas(426, 240);
@@ -21,6 +22,35 @@ function modelLoaded() {
     status = true;
 }
 
+
 function draw() {
-    image(video, 0, 0, 426, 240);
+    image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(gotResult);
+        video.stop();
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("object_found").innerHTML = "object found";
+   
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            var synth = window.speechSynthesis;
+            speak_data = objects[i].label;
+        }
+    }
 }
+function gotResult(error, results) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(results);
+    objects = results;
+  }
+  
+
